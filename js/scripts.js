534a779d5aba00de115288a5e3ab610b445a05f1
Mariia <mariiapopovych@gmail.com>
2019-04-07 12:59:00 -0700

    change js logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,37 @@
-function Ticker(title, time, age) {
+//Business Logic
+
+
+function Ticket(title, time, age) {
   this.title = title,
     this.time = time,
-    this.ahe = age
+    this.age = age
 }
+
+Ticket.prototype.calculatePrice = function() {
+  // var basePrice = 5;
+  if (this.title === "Harry Potter" && this.time === "04:00 PM" && this.age === "adult") {
+    return "$5";
+  } else {
+    return "$3";
+  }
+}
+
+
+
+// UI Logic
+$(document).ready(function() {
+  $("#buy-ticket").click(function(event) {
+    event.preventDefault();
+    var movieTitle = $("input:radio[name=movie]:checked").val();
+    $("#confirm").click(function() {
+      var inputAge = $("input:radio[name=age]:checked").val();
+      var inputTime = $("input:radio[name=time]:checked").val();
+      var ticket = new Ticket(movieTitle, inputTime, inputAge);
+      $("#price").text(ticket.calculatePrice());
+
+
+    });
+
+  });
+
+});
